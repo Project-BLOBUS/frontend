9a80e1028ba339776248cc49da045e294b76af65
jun <grow9818@naver.com>
2024-11-18 11:10:10 +0900

    11/18일

--- src/main/Footer.js
+++ src/main/Footer.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-        <footer className="w-full sm:w-[90%] lg:w-[70.6%] mx-auto mt-2 py-4 bg-[linear-gradient(45deg,_#6E00FF,_#DB0153,#0130BC)] text-white">
+        <footer className="w-full sm:w-[90%] lg:w-[70.7%] sm:ml-[15%] mx-auto mt-2 py-4 bg-[linear-gradient(45deg,_#6E00FF,_#DB0153,#0130BC)] text-white">
             <div className="flex flex-col sm:flex-row justify-between items-center sm:space-y-0 space-y-4 sm:space-x-6">
                 <div className="text-center sm:text-left">
                     <h3 className="text-lg font-semibold">회사 이름 : BLOBUS</h3>
                     <p className="text-sm">© 2024 Company Name. All rights reserved.</p>
                 </div>
                 <div className="flex sm:flex-row flex-col sm:space-x-6 sm:mt-0 mt-4 sm:text-left text-center">
-                    <Link to="/#" className="hover:text-gray-300 transition duration-500">About Us</Link>
-                    <Link to="/#" className="hover:text-gray-300 transition duration-500">Contact</Link>
-                    <Link to="/#" className="hover:text-gray-300 transition duration-500">Privacy Policy</Link>
-                    <Link to="/#" className="hover:text-gray-300 transition duration-500">Terms of Service</Link>
+                    <Link to="/#" className="hover:text-gray-300 transition duration-500 mt-8">About Us</Link>
+                    <Link to="/#" className="hover:text-gray-300 transition duration-500 mt-8">Contact</Link>
+                    <Link to="/#" className="hover:text-gray-300 transition duration-500 mt-8">Privacy Policy</Link>
+                    <Link to="/#" className="hover:text-gray-300 transition duration-500 mt-8">Terms of Service</Link>
                 </div>
                 <div className="text-center sm:text-right">
                     <p className="text-sm">Follow us:</p>
