283adcb3be4e2361b6ad2f66e4b89ca6075cb954
yang4916 <park49164357@gmail.com>
2024-12-05 17:18:07 +0900

    241205 1717 수정/테스트용 코드 삭제

--- src/member/component/find/FindPw.js
+++ src/member/component/find/FindPw.js
@@ -8,10 +8,10 @@ import Loading from "../../../etc/component/Loading";
 
 const initState = {
   // ToDEL 삭제
-  userId: "bell4916@naver.com",
+  userId: "",
   authCode: "",
-  userPw: "qwerQWER1234!@#$",
-  confirmPw: "qwerQWER1234!@#$",
+  userPw: "",
+  confirmPw: "",
   roleName: "GENERAL",
 };
 
@@ -173,8 +173,6 @@ const FindPw = () => {
                       authCode: code,
                     });
                     toast.success("메일 전송 성공");
-                    // ToDEL 삭제
-                    setMember({ ...member, authCode: code });
                   } catch (error) {
                     toast.error("메일 전송에 실패했습니다.");
                   }
